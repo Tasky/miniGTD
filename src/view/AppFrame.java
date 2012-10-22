1aa1659da4a27330a68d9c742a03a129eb36bf11
Tim Slot <tim.slot@gmail.com>
2012-10-22 20:35:51 +0200

    added layout constants

---
+++ src/view/AppFrame.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author tim
+ */
+public class AppFrame extends JFrame {
+    
+}
