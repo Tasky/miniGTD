d854cffe8dc5cbd241a41cc328643424475be233
Tim Slot <tim.slot@gmail.com>
2012-10-29 21:09:50 +0100

    added models.Status

---
+++ src/models/Status.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import util.DataLayer;
+import util.exceptions.ConnectionException;
+
+/**
+ *
+ * @author tim
+ */
+public class Status {
+    
+    private int id;
+    private String name;
+
+    
+    public static List<Status> all() throws ConnectionException {
+        List<Status> list = new ArrayList<Status>();
+        PreparedStatement statement = null;
+            
+        try{
+
+            statement = DataLayer.getConnection().prepareStatement("SELECT id, name FROM statuses");
+            statement.execute();
+            ResultSet res = statement.getResultSet();
+            statement.close();
+
+            while(res.next()) {
+                Status t = new Status();
+                t.fromResultSet(res);
+                list.add(t);
+            }
+            return list;
+        }catch(SQLException e) {
+            throw new ConnectionException();
+        }
+    }
+    
+    private void fromResultSet(ResultSet rs) throws SQLException {
+        this.id = rs.getInt("id");
+        this.name = rs.getString("name");
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public int getId() {
+        return this.id;
+    }
+}
