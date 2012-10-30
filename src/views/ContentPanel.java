be1f8e790c6959f41cccbea94b6d7fa66837d08b
Nanne Wielinga <nanne@mycel.nl>
2012-10-30 16:17:13 +0100

    Add sorting to ContentPanel, changes needed in controller.

--- src/views/ContentPanel.java
+++ src/views/ContentPanel.java
@@ -13,6 +13,9 @@ import views.content.ThoughtItem;
 import views.content.ThoughtForm;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.*;
 
@@ -52,7 +55,7 @@ public class ContentPanel extends JPanel {
         repaint();
     }
 
-    public void showTasks(List<Task> tasks, boolean formVisible) {
+    public void showTasks(List<Task> tasks, Task.Sort currentSort, boolean asc, boolean formVisible) {
         removeAll();
 
         if (formVisible) {
@@ -61,8 +64,47 @@ public class ContentPanel extends JPanel {
             add(form, "span, growx");
         }
 
+        JPanel sortPanel = new JPanel();
+        sortPanel.setOpaque(false);
+        sortPanel.setLayout(new MigLayout("insets 0 n 0 n", "[grow][][][][][]", ""));
+        sortPanel.add(new JLabel(""));
+        sortPanel.add(new JLabel("Sorteren op:"));
+
+        class Sort {
+            public final String name;
+            public final Task.Sort sort;
+            public Sort(String name, Task.Sort sort) {
+                this.name = name;
+                this.sort = sort;
+            }
+        }
+        List<Sort> sorts = new ArrayList<Sort>();
+        sorts.add(new Sort("Volgorde", Task.Sort.ORDER));
+        sorts.add(new Sort("Actie", Task.Sort.TASK));
+        sorts.add(new Sort("Einddatum", Task.Sort.ACTIONDATE));
+        sorts.add(new Sort("Status", Task.Sort.STATUS));
+
+        for(final Sort sort : sorts) {
+            String name = sort.name;
+            if(currentSort.equals(sort.sort))
+                if(asc)
+                    name += " ↓";
+                else
+                    name += " ↑";
+            JButton jButton = new JButton(name);
+            jButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    controller.sort(sort.sort);
+                }
+            });
+            sortPanel.add(jButton);
+        }
+
+        add(sortPanel, "span, growx");
+
         JPanel holder = new JPanel();
-        holder.setLayout(new MigLayout("gap 20px", "[grow]", "[]"));
+        holder.setLayout(new MigLayout("insets 0 n n n, gap 20px", "[grow]", "[]"));
         holder.setOpaque(false);
         for(models.Task task : tasks) {
             holder.add(new TaskItem(controller, task), "span, growx");
